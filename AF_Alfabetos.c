1bd4914f78d69cbeb9f60a236e17311a8880195a
Akemi <akemirodgo@gmail.com>
2024-10-24 14:35:50 -0800

    Update AF_Alfabetos.c

--- AF_Alfabetos.c
+++ AF_Alfabetos.c
@@ -1,46 +1,46 @@
 #include <stdio.h>
 
-int main(){
-    char A[]= {1,3,5,7};
-    char U[]= {1,2,3,4,5,6,7,8};
-    int tamA=4;  
-    int tamU=8;  
-    int unionAlf[12]; 
-    int diferencia[4]; 
-    int tamUnion=0;  
-    int tamDiferencia=0;  
-    int i, j;
-    int encontrado;
+void unionC(char a[], int tA, char u[], int tU);  
+void diferencia(char a[], int tA, char u[], int tU);      
+int encontrado(char a[], int tA, char u);         
 
-    for(i=0; i < tamA; i++){
-        unionAlf[tamUnion]= A[i];
-        tamUnion++;
-    }
-    for(i=0; i < tamU; i++){
-        unionAlf[tamUnion]= U[i];
-        tamUnion++;
-    }
+int main(){
+    char A[]= {1, 3, 5, 7};  
+    char U[]= {1, 2, 3, 4, 5, 6, 7, 8};  
+    int tA = 4;  
+    int tU= 8;  
+
     printf("Union de A y U: ");
-    for(i=0; i < tamUnion; i++){
-        printf("%d ", unionAlf[i]);
-    }
-    printf("\n");
-    for(i=0; i < tamA; i++){
-        encontrado= 0;
-        for(j=0; j< tamU; j++){
-            if(A[i]== U[j]){
-                encontrado = 1;
-            }
-        }
-        if(!encontrado){
-            diferencia[tamDiferencia]= A[i];
-            tamDiferencia++;
-        }
-    }
-    printf("Diferencia A - U: ");
-    for(i= 0; i< tamDiferencia; i++){
-        printf("%c ", diferencia[i]);
-    }
-    printf("\n");
+    unionC(A, tA, U, tU);  
+    printf("\nDiferencia A - U:");
+    diferencia(A, tA, U, tU);      
     return 0;
+}
+void unionC(char a[], int tA, char u[], int tU){
+    int i;
+    for(i= 0; i< tA; i++){
+        printf("%i ", a[i]);
+    }
+    for(i= 0; i< tU; i++){
+        if(!encontrado(a, tA, u[i])){  
+            printf("%i ", u[i]);
+        }
+    }
+}
+void diferencia(char a[], int tA, char u[], int tU){
+    int i;
+    for(i= 0; i< tA; i++){
+        if(!encontrado(u, tU, a[i])){  
+            printf("%i ", a[i]);
+        }
+    }
+}
+int encontrado(char a[], int tA, char u){
+    int i;
+    for(i= 0; i< tA; i++){
+        if(a[i]== u){
+            return 1;  
+        }
+    }
+    return 0;  
 }
